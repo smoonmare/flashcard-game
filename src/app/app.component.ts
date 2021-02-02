4adedabad3c4f6e762dadbebe929ebba85a189ff
smoonmare <smoonmare@gmail.com>
2021-02-02 14:23:44 -0800

    Creating an array of flashes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { IFlash } from './flash.model';
 
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10000);
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flashcard-game';
+  flashs: IFlash[] = [{
+    question: 'Question 1',
+    answer: 'Answer 1',
+    show: false,
+    id: getRandomNumber(),
+  }, {
+    question: 'Question 2',
+    answer: 'Answer 2',
+    show: false,
+    id: getRandomNumber(),
+  } {
+    question: 'Question 3',
+    answer: 'Answer 3',
+    show: false,
+    id: getRandomNumber(),
+  }];
+
+  trackByFlashId(index, flash) {
+    return flash.id;
+  }
 }
