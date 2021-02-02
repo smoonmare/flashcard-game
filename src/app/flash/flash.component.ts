c9aa4d7bc75dcd9c3d1c53bdaadf90d05e8305fd
smoonmare <smoonmare@gmail.com>
2021-02-02 14:04:04 -0800

    Added Flash property as Input to flash component

--- src/app/flash/flash.component.ts
+++ src/app/flash/flash.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IFlash } from './../flash.model';
 
 @Component({
   selector: 'app-flash',
   templateUrl: './flash.component.html',
   styleUrls: ['./flash.component.css']
 })
-export class FlashComponent implements OnInit {
+export class FlashComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input() flash: IFlash = {
+    id: 1,
+    question: 'React to Angular',
+    answer: 'No Reaction XD',
+    show: false,
+  };
 
 }
